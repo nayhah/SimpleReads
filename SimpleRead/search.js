782672cb9fb4c205de71a4e31090192b567aca8b
Nayha H <105979099+nayhah@users.noreply.github.com>
2023-04-11 10:41:15 -0400

    Update search.js

--- SimpleRead/search.js
+++ SimpleRead/search.js
@@ -12,15 +12,55 @@ function searchKeywords(keyword) {
     return book_data
 }
 
-function searchAuthor(author) {
-    api = "https://www.googleapis.com/books/v1/volumes?q=inauthor:"
+    function searchAuthor(author) {
+        const response = fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=inauthor:${encodeURIComponent(
+            author
+          )}`
+        );
+
+        return response.then((r) => r.json());
+      }
+
+      const list = document.querySelector("#searchResults");
+      //# id for textbox
+      //query selector lets you selects things by css id
+      const authorTextbox = document.querySelector("#authorTextbox");
+      const searchButton = document.querySelector("#searchButton");
+
+      searchButton.addEventListener("click", async () => {
+
+        const author = authorTextbox.value;
+        const result = await searchAuthor(authorTextbox.value);
+
+        console.log(result);
+
+        const results = result.items.slice(0, 5);
+
+        for (const result of results) {
+          const li = document.createElement("li");
+          const a = document.createElement("a");
+
+          a.href = result.volumeInfo.previewLink;
+          a.textContent = result.volumeInfo.title;
+          a.target = "_blank";
+          a.rel = "noopener noreferrer";
+
+          li.appendChild(a);
+          list.appendChild(li);
+        }
+      });
+    /*api = "https://www.googleapis.com/books/v1/volumes?q=inauthor:"
+    var authorTextbox = document.getElementById("authorTextbox").value;
 
     // send a request and get a JSON response
-    resp = urlopen(api + removeSpaces(author))
+    //get data at runtime
+    const data
+    resp = fetch(api + removeSpaces(authorTextbox))
     // parse JSON into Python as a dictionary
     book_data = json.load(resp)
     return book_data
-}
+    */
 
 function searchGenre(subject) {
     api = "https://www.googleapis.com/books/v1/volumes?q=subject:"
@@ -37,3 +77,7 @@ function jsonWrite(book_data) {
     f.write(json.dumps(book_data))
     f.close()
 }
+/*<input type="text" id="userInput">
+let userInput = document.getElementById("userInput").value;
+*/
+
